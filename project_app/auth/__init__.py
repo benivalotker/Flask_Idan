295be2a8a8ea4f135a44c9023d7db32d6f3370ff
BeniV+1-at-445511888825 <xP+JrFpBTNRjvHQVJxe0OyVJ0YyCHh/iq77QwpUhNs8=>
2021-12-27 12:56:05 +0200

    add disconnect route

--- project_app/auth/__init__.py
+++ project_app/auth/__init__.py
@@ -1,9 +1,10 @@
 """
     local application register
 """
-from project_app.auth.user_login import user_login
+from project_app.auth.user_login import user_login, user_disconnect
 
 
 def register(app):
     # test_request blueprint
     app.register_blueprint(user_login, url_prefix='/user_login')
+    app.register_blueprint(user_disconnect, url_prefix='/user_login')
--- project_app/auth/user_login.py
+++ project_app/auth/user_login.py
@@ -2,6 +2,7 @@ import config
 from flask import Blueprint, request, redirect
 
 user_login = Blueprint(name='user_login', import_name=__name__)
+user_disconnect = Blueprint(name='user_disconnect', import_name=__name__)
 
 
 @user_login.route('/', methods=['GET'])
@@ -20,3 +21,15 @@ def index():
     else:
         # TODO: print the message in the html file
         return "<h1>login failed</h1>"
+
+
+@user_disconnect.route('/user_disconnect', methods=['GET'])
+def index():
+    # TODO: remove connect session and redirect to login page
+    # body request data
+    data = request.json
+
+    print("user disconnect")
+
+    return {"operation": "disconnect"}
+
