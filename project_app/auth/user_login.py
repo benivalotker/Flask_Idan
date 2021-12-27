83637faf502f94c507d01b81aad1407d74d7c62c
BeniV+1-at-445511888825 <xP+JrFpBTNRjvHQVJxe0OyVJ0YyCHh/iq77QwpUhNs8=>
2021-12-27 13:40:08 +0200

    create templates folder + base.html + user_login.html

--- project_app/auth/user_login.py
+++ project_app/auth/user_login.py
@@ -1,12 +1,13 @@
 import config
-from flask import Blueprint, request, redirect
+from flask import Blueprint, request, redirect, render_template
 
-user_login = Blueprint(name='user_login', import_name=__name__)
-user_disconnect = Blueprint(name='user_disconnect', import_name=__name__)
+user_login = Blueprint(name='user_login', import_name=__name__, template_folder='templates')
+user_disconnect = Blueprint(name='user_disconnect', import_name=__name__, template_folder='templates')
 
 
 @user_login.route('/', methods=['GET'])
 def index():
+    title = "Login"
     # body request data
     data = request.json
 
@@ -16,11 +17,9 @@ def index():
             # redirect to home page
             return redirect(f"{config.base_url}/home_page/", code=307)
         else:
-            # TODO: print the message in the html file
-            return "<h1>login failed</h1>"
+            return render_template("login/templates.login_page", data={"msg": "Login Error"}, title=title)
     else:
-        # TODO: print the message in the html file
-        return "<h1>login failed</h1>"
+        return render_template("login/login_page.html", data={"msg": "Login Error"}, title=title)
 
 
 @user_disconnect.route('/user_disconnect', methods=['GET'])
